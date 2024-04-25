d65029474682478207c6ac90b8adc588497a0fd8
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-25 09:00:39 +0300

    all props and componenets added

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from"./Links"
 
-function About() {
+function About(props){
+  function Isbio (props){ 
+  if (props.bio){
+    return <p>{props.bio}</p>
+  } 
+ return  null
+}
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      < Isbio bio={props.bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {console.log(props.links)}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
